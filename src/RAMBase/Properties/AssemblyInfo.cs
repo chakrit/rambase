40bbbc4d366e9831eebaa2e59ffc86bd0e44be6b
Chakrit Wichian <chakrit@hlpth.com>
2011-09-27 13:16:24 +0700

    Initial project creation.

---
+++ src/RAMBase/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RAMBase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("RAMBase")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("82ba5778-6955-47d6-bb66-d83cb6f9ab7a")]
+
+[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyFileVersion("0.0.0.*")]
